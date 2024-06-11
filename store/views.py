b7b36a96b23c33099b8d612fe4a3bde9e81f60e5
AJAFF7 <a.j.s-engineer@outlook.com>
2024-06-11 14:30:22 +0300

    Update views.py

--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ def home(request):
         if product.price and product.sale_price:
             product.discount_percentage = ((product.price - product.sale_price) / product.price) * 100
         else:
-            product.discount_percentage = 0
+            product.discount_percentage = "Zero"
     return render(request, 'home.html', {'products': products})
 
 def login_user(request):
