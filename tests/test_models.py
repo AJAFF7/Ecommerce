60014dfc3df937ca82f14b8bbabdffb20dcc4b77
AJAFF7 <a.j.s-engineer@outlook.com>
2024-06-11 16:01:37 +0300

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,15 @@
-# from django.test import TestCase
-# from store.models import Product, Category
+from django.test import TestCase
+from store.models import Product, Category
 
-# class TestProductModel(TestCase):
+class TestProductModel(TestCase):
 
-#     def setUp(self):
-#         # Create a category
-#         self.category = Category.objects.create(name='Test Category')
+    def setUp(self):
+        # Create a category
+        self.category = Category.objects.create(name='Test Category')
 
-#     def test_create_product(self):
-#         # Create a product with a category
-#         product = Product.objects.create(name='Test Product', price=10, category=self.category)
+    def test_create_product(self):
+        # Create a product with a category
+        product = Product.objects.create(name='Test Product', price=10, category=self.category)
 
-#         # Assert that the product was created successfully
-#         self.assertIsNotNone(product)
+        # Assert that the product was created successfully
+        self.assertIsNotNone(product)
