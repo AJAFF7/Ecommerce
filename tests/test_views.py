1ad9680478747eecd8a1360a3c048b07c0f2119a
AJAFF7 <a.j.s-engineer@outlook.com>
2024-06-14 13:00:21 +0300

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -30,8 +30,8 @@ class TestViews(TestCase):
         self.client = Client()
         self.home_url = reverse('home')
         self.login_url = reverse('login')
-        self.username = 'jaff'
-        self.password = 'jaff5550140'
+        self.username = 'testuser'
+        self.password = 'testpass123'
         self.user = User.objects.create_user(username=self.username, password=self.password)
 
     def test_home_view(self):
@@ -55,15 +55,15 @@ class TestViews(TestCase):
         self.assertRedirects(response, self.home_url)  # Assuming successful login redirects to home page
 
     def test_login_view_post_incorrect_credentials(self):
-    response = self.client.post(self.login_url, {
-        'username': self.username,
-        'password': 'wrongpassword'
-    })
-    self.assertEqual(response.status_code, 302)  # Check if it redirects
-    redirected_response = self.client.get(response.url)  # Follow the redirect
-    self.assertEqual(redirected_response.status_code, 200)  # Ensure the redirected page is the login page
-    self.assertTrue('login.html' in [t.name for t in redirected_response.templates])  # Check if 'login.html' is used
-    self.assertContains(redirected_response, 'Invalid login')  # Assuming an 'Invalid login' message is shown
+        response = self.client.post(self.login_url, {
+            'username': self.username,
+            'password': 'wrongpassword'
+        })
+        self.assertEqual(response.status_code, 302)  # Check if it redirects
+        redirected_response = self.client.get(response.url)  # Follow the redirect
+        self.assertEqual(redirected_response.status_code, 200)  # Ensure the redirected page is the login page
+        self.assertTrue('login.html' in [t.name for t in redirected_response.templates])  # Check if 'login.html' is used
+        self.assertContains(redirected_response, 'Invalid login')  # Assuming an 'Invalid login' message is shown
 
 
 
