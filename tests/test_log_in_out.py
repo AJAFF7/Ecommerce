15bac259be2cefa46f38e4fe6784139a22c6f612
AJAFF7 <a.j.s-engineer@outlook.com>
2024-07-04 15:04:17 +0300

    Update test_log_in_out.py

--- tests/test_log_in_out.py
+++ tests/test_log_in_out.py
@@ -7,7 +7,7 @@ class TestDjangoApp(unittest.TestCase):
         # Use webdriver_manager to manage GeckoDriver
         options = webdriver.FirefoxOptions()
         options.headless = True  # Run Firefox in headless mode (optional)
-        self.driver = webdriver.Firefox(options=options, service=GeckoDriverManager().install())
+        self.driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
 
     def tearDown(self):
         self.driver.quit()
