d99ec9ebaba51a86c2da0c5c2b6b310ceeef34d3
AJAFF7 <a.j.s-engineer@outlook.com>
2025-04-18 13:30:28 +0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-kl8ms^qq+(=vnxnf(5e+-t80y)w3=y_y)b!u18^nq0d&r97%@r'
@@ -13,18 +10,29 @@ SECRET_KEY = 'django-insecure-kl8ms^qq+(=vnxnf(5e+-t80y)w3=y_y)b!u18^nq0d&r97%@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.3', '192.168.1.5', 'crm.ajs-engineer.com', 'ajs-engineer.com', '172.18.0.2', '172.19.0.121']
-
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://crm.ajs-engineer.com',
-    'https://localhost',  # if you need it for local testing
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '192.168.0.3',
+    '192.168.1.5',
+    'crm.ajs-engineer.com',
+    'www.crm.ajs-engineer.com',
+    'ajs-engineer.com',
+    '172.18.0.2',
+    '172.19.0.121',
 ]
 
+# Trust your frontend domains for CSRF protection
+CSRF_TRUSTED_ORIGINS = [
+    'https://crm.ajs-engineer.com',
+    'https://www.crm.ajs-engineer.com',
+    'https://localhost',
+]
 
+# Tell Django that HTTPS is used when behind a proxy
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
-    'cart'
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,10 +75,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -78,43 +83,26 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
+# Static and Media files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
